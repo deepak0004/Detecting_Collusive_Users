400f33a77b719d6dc291d772e18f319438b619e1
deepak0004 <deepak14036@iiitd.ac.in>
2018-02-28 21:42:04 +0530

    Pushing code

---
+++ remove_dup.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+map<string, int> ma;
+int main()
+{
+    #ifndef  ONLINE_JUDGE
+      freopen("input.txt", "r", stdin);
+    #endif
+
+    string st;
+    int N, ans = 0, i;
+
+    cin >> N;
+    for(i=1; i<=N; i++)
+    {
+       cin >> st;
+       if( !ma[st] )
+       {
+           ma[st] = 1;
+           ans++;
+       }
+       else
+       {
+           cout << i << endl;
+       }
+    }
+    cout << ans;
+
+    return 0;
+}
